9b2e9ff463b800350b874738c6e2f859b073fab1
Jorge Enrique Leporace <67753773+jorgeleporace@users.noreply.github.com>
2022-11-08 09:49:04 -0300

    Se agregaron Objetos Arrays

--- index.js
+++ index.js
@@ -3,35 +3,27 @@ const txtOperacion = document.getElementById("operacion")
 const txtOp2 = document.getElementById("op2")
 const btnCalcular = document.getElementById("calcular")
 const pResultado = document.getElementById("resultado")
-
+const arrayOperaciones = [];
 btnCalcular.addEventListener('click', calcular)
 
 function calcular(){
     const operacion = txtOperacion.value
     const op1 = parseFloat(txtOp1.value)
     const op2 = parseFloat(txtOp2.value)
-
+    const operation = new Operation(op1,operacion,op2)
     if((operacion == "+" || operacion == "-" || operacion == "*" || operacion == "/") && !isNaN(op1) && !isNaN(op2)){
-        let resultado;
-        switch (operacion){
-            case "+":
-                resultado = op1 + op2
-                break;
-            case "-":
-                resultado = op1 - op2
-                break;
-            case "*":
-                resultado = op1*op2
-                break;
-            case "/":
-                resultado = op1/op2
-                break
-        }
+        
+        let resultado = operation.calcular();
+        arrayOperaciones.push(operation);
         pResultado.style = "color:black"
         pResultado.innerText = "= " + resultado
     }else{
         pResultado.style = "color:red"
         pResultado.innerText = "Calculo imposible"
     }
+const primerArray = ["suma", "resta", "multipilicación", "división" ];
+const count = primerArray.push("suma");
+console.log(count);
+console.log(arrayOperaciones)
 }
 
